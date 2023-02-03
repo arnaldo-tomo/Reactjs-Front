8135a16e1b63f18f080cae9543dea25c6dbcfdd4
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2023-02-03 13:05:27 +0200

    Update Home.js

--- my/src/views/Home.js
+++ my/src/views/Home.js
@@ -3,7 +3,24 @@ import React, { Component } from "react";
 class Home extends Component {
     render() {
         return (
-            <h1>Hello Word</h1>
+            <div className="container row py-4">
+                <div className="col-md-12 col-sm-6">
+                    <div className="card">
+                        <div className="card-header">
+                            <div className="row">
+                                <div className="col-6">
+                                    <strong> ReacJS Front</strong>
+                                </div>
+                                <div className="col-6">
+                                    <a className="btn btn-success btn-sm float-end"><i className="fa fa-plus"></i> Add New Student </a>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+
+            </div>
         );
     }
 }
