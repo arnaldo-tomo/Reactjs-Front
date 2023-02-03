1d3c17bb9c0b6bcb922e265570473edcad2ecf1f
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2023-02-03 12:48:02 +0200

    Create sobre.js

---
+++ my/src/pages/sobre.js
@@ -0,0 +1,14 @@
+
+import React, { Component } from "react";
+
+class sobre extends Component {
+    render() {
+        return (
+            <div>
+                <h5>SObre</h5>
+            </div>
+        );
+    }
+}
+
+export default sobre;
