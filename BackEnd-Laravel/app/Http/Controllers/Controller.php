3cbb926cde37f9f9958ac607c4c333ea53a9f5d8
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2023-02-03 14:50:19 +0200

    Update Controller.php

--- BackEnd-Laravel/app/Http/Controllers/Controller.php
+++ BackEnd-Laravel/app/Http/Controllers/Controller.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\student;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
@@ -10,4 +11,19 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function salvar(Request $request)
+    {
+        $dado = new student;
+        $dado->nome = $request->input('nome');
+        $dado->curso = $request->input('curso');
+        $dado->email = $request->input('email');
+        $dado->telefone = $request->input('telefone');
+        $dado->save();
+
+        return  response()->json([
+            'status', 200,
+            'message' => 'estudante salvo com sucessos',
+        ]);
+    }
 }
