1f012440b6425c42722a601c917b456af41e97c3
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2023-02-03 10:24:33 +0200

    sei la

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import logo from './logo.svg';
 import React from 'react';
 import { browserRouter as Router, Switch, Route } from 'react-router-dom';
 
+
+import student from './pages/student';
+import Addstudent from './pages/Addstudent';
+
 function App() {
   return (
 
     <Router>
       <Switch>
-
+        <Route exact path="/" Component={student} />
+        <Route exact path="addstudent " Component={Addstudent} />
       </Switch>
     </Router>
 
