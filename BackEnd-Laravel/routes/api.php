87f2adfdca19c92f8aa47cf1eacf3774d39644ae
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2023-02-04 08:50:44 +0200

    Update api.php

--- BackEnd-Laravel/routes/api.php
+++ BackEnd-Laravel/routes/api.php
@@ -1,7 +1,8 @@
 <?php
 
 use App\Http\Controllers\Controller;
+use App\Http\Controllers\ControllerStudent;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/salvar', [Controller::class, 'salvar']);
+Route::post('/salvar', [ControllerStudent::class, 'salvar']);
