133baf7f90f977eaacd1f5a390c98ae5ec49fa1d
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2023-02-04 09:15:59 +0200

    Update 2023_02_03_122331_create_students_table.php

--- BackEnd-Laravel/database/migrations/2023_02_03_122331_create_students_table.php
+++ BackEnd-Laravel/database/migrations/2023_02_03_122331_create_students_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->string('nome');
             $table->string('curso');
             $table->string('email');
-            $table->integer('telefone');
+            $table->string('telefone');
             $table->timestamps();
         });
     }
