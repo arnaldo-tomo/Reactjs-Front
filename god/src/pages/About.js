1b3ad516ce95d69b9debfc6cfc9157df97f8e831
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2023-02-05 13:44:21 +0200

    Update About.js

--- god/src/pages/About.js
+++ god/src/pages/About.js
@@ -6,7 +6,21 @@ class About extends Component {
             <>
                 <Menu />
                 <div className="container">
-                    <h1>About</h1>
+                    <div className=" d-flex justify-content-center">
+                        <div className="row col-md-6 col-sm-6 py-4 ">
+                            <div className="col-12 ">
+                                <div className="card ">
+                                    <div className="card-header">
+                                        <strong>Consumindo APi</strong>
+                                        <hr></hr>
+                                        <p>#2- Nessa vídeo aula daremos continuidade criando os componentes da tela de Login e criaremos as funções responsáveis pelo login utilizando Axios e Redux.
+                                        </p>
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </>
         );
