a5bceb20687f3edd8c31dd37465fc571078a65b0
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2023-02-05 10:21:25 +0200

    Update Home.js

--- god/src/pages/Home.js
+++ god/src/pages/Home.js
@@ -1,17 +1,14 @@
 import { Component } from "react";
 import { Link } from 'react-router-dom';
+import Menu from "./components/Menu";
+
 
 class Home extends Component {
     render() {
         return (
-            <div>
-                <h1>PAGINa HOME</h1>
-                <ul>
-                    <Link to={'/'} ><li>welcome</li> </Link>
-                    <Link to={'About'}><li>About</li> </Link>
-                    <a href="About" ><li>Home</li> </a>
-                </ul>
-            </div>
+            <>
+                <Menu />
+            </>
         );
     }
 }
