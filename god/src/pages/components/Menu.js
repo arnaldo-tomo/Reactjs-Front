d96b310cad189f40ba53f478da375526cb0a5238
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2023-02-05 12:58:20 +0200

    Update Menu.js

--- god/src/pages/components/Menu.js
+++ god/src/pages/components/Menu.js
@@ -1,9 +1,28 @@
 import { Component } from "react";
+import About from './../About';
 class Menu extends Component {
+
     render() {
+        const nab = "{ Crud ReactJS }"
+        const login = "Login "
         return (
-            <div>
-                <h1>Menu</h1>
+            <div className="container shadow-2">
+                <nav className="navbar bg-light shadow-2" data-bs-theme="light" className="navbar navbar-expand-lg bg-body-tertiary">
+                    <div className="container-fluid">
+                        <a className="btn btn-primary btn-sm" href="/">{nab}</a>
+                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                            <span className="navbar-toggler-icon"></span>
+                        </button>
+                        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                            <div className="navbar-nav">
+                                <a className="nav-link active" aria-current="page" href="/Home">Home</a>
+                                <a className="nav-link active" aria-current="page" href="/About" >About</a>
+
+                            </div>
+                        </div>
+                        <a className="btn btn-success btn-sm reunded-0" href="/">{login}</a>
+                    </div>
+                </nav>
             </div>
         );
     }
