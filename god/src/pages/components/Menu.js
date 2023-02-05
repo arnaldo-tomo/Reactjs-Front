143811946b3c37f9490caa0322b6d3ac78a75f77
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2023-02-05 13:44:24 +0200

    Update Menu.js

--- god/src/pages/components/Menu.js
+++ god/src/pages/components/Menu.js
@@ -4,23 +4,23 @@ class Menu extends Component {
 
     render() {
         const nab = "{ Crud ReactJS }"
-        const login = "Login "
+        const login = "Add new Post "
         return (
             <div className="container shadow-2">
-                <nav className="navbar bg-light shadow-2" data-bs-theme="light" className="navbar navbar-expand-lg bg-body-tertiary">
+                <nav className="navbar bg-light" data-bs-theme="dark" className="navbar navbar-expand-lg bg-body-tertiary">
                     <div className="container-fluid">
                         <a className="btn btn-primary btn-sm" href="/">{nab}</a>
                         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                             <span className="navbar-toggler-icon"></span>
                         </button>
-                        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                        <div className="collapse float-end navbar-collapse" id="navbarNavAltMarkup">
                             <div className="navbar-nav">
                                 <a className="nav-link active" aria-current="page" href="/Home">Home</a>
                                 <a className="nav-link active" aria-current="page" href="/About" >About</a>
 
                             </div>
                         </div>
-                        <a className="btn btn-success btn-sm reunded-0" href="/">{login}</a>
+                        <a className="btn btn-success btn-sm d-felx justify-content-end" href="/Home">{login}</a>
                     </div>
                 </nav>
             </div>
