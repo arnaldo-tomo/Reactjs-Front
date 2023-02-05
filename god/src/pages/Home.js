284c48f66b218f517c54743b0c87740b4bb41dcc
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2023-02-05 12:58:22 +0200

    Update Home.js

--- god/src/pages/Home.js
+++ god/src/pages/Home.js
@@ -8,6 +8,9 @@ class Home extends Component {
         return (
             <>
                 <Menu />
+                <div className="container ">
+                    <h1>Home</h1>
+                </div>
             </>
         );
     }
