bdc4fbdb0f56a473997c73e931b1bc69b8b63113
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2023-02-05 10:21:23 +0200

    Create Menu.js

---
+++ god/src/pages/components/Menu.js
@@ -0,0 +1,12 @@
+import { Component } from "react";
+class Menu extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Menu</h1>
+            </div>
+        );
+    }
+}
+
+export default Menu;
