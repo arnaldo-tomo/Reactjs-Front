3c75a2af8a771b33adfea91cb7ec7e356256dedc
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2023-02-05 12:58:17 +0200

    Update App.js

--- god/src/App.js
+++ god/src/App.js
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home'
 import About from './pages/About';
+import Welcome from './pages/Welcome';
 function App() {
   return (
 
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Home />} />
+        <Route path='/' element={<Welcome />} />
+        <Route path='/Home' element={<Home />} />
         <Route path='/About' element={<About />} />
       </Routes>
     </BrowserRouter>
