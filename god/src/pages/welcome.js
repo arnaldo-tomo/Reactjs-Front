3a84144a0580e8ad596685c99d32d43a1989c3ce
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2023-02-05 13:44:28 +0200

    Update welcome.js

--- god/src/pages/welcome.js
+++ god/src/pages/welcome.js
@@ -5,7 +5,25 @@ export default function Welcome() {
         <>
             <Menu />
             <div className="container">
-                <h1>Welcome</h1>
+                <div className=" d-flex justify-content-center">
+                    <div className="row col-md-6 col-sm-6 py-4 ">
+                        <div className="col-12 ">
+                            <div className="card ">
+                                <div className="card-header">
+                                    <strong>Consumindo APi</strong>
+                                    <hr></hr>
+                                    <p>#2- Nessa vídeo aula daremos continuidade criando os componentes da tela de Login e criaremos as funções responsáveis pelo login utilizando Axios e Redux.
+                                    </p>
+                                    <div className="m-2 d-flex justify-content-between">
+                                        <a className="btn btn-success btn-sm m-2">EDITAR</a>
+                                        <a className="btn btn-info btn-sm m-2">Ler MaIs</a>
+                                        <a className="btn btn-danger btn-sm m-2">Deletar</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </>
     );
