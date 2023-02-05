c23778c61603e2048aee3df576603612c58a5c18
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2023-02-05 10:15:31 +0200

    Create App.js

---
+++ god/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home'
+import About from './pages/About';
+function App() {
+  return (
+
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/About' element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
