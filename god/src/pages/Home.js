0164ab8e00133d2b9f4aace455e6d79bd83c86e3
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2023-02-05 13:44:26 +0200

    Update Home.js

--- god/src/pages/Home.js
+++ god/src/pages/Home.js
@@ -9,7 +9,28 @@ class Home extends Component {
             <>
                 <Menu />
                 <div className="container ">
-                    <h1>Home</h1>
+                    <div className="container">
+                        <div className=" d-flex justify-content-center">
+                            <div className="row col-md-6 col-sm-6 py-4 ">
+                                <div className="col-12 ">
+                                    <div className="card ">
+                                        <div className="card-header">
+                                            <strong>Criar dados</strong><Link to={'/'} className="btn btn-info btn-sm float-end">Voltar</Link>
+                                            <hr></hr>
+                                            <label>Nome</label>
+                                            <input type={'search'} placeholder="Infome nome " className="form-control"></input>
+                                            <label>Desricao</label>
+                                            <textarea className="form-control" rows={5}></textarea>
+                                            <a className="btn btn-success btn-sm m-2">Salvar</a>
+                                            <div className=" d-flex justify-content-between">
+
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </>
         );
