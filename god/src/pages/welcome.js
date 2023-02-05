81ef6a01e2cba5eb1390cb7f74fce0553d1198bd
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2023-02-05 10:21:26 +0200

    Update welcome.js

--- god/src/pages/welcome.js
+++ god/src/pages/welcome.js
@@ -3,12 +3,7 @@ class welcome extends Component {
     render() {
         return (
             <>
-                <div>Pagina Welcome</div>
-                <ul>
-                    <Link to={'/'} ><li>welcome</li> </Link>
-                    <Link to={'About'}><li>About</li> </Link>
-                    <a href="About" ><li>Home</li> </a>
-                </ul>
+
             </>
 
 
