8d8fcdfad587fe53ea48a6a276b830dc435bafca
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2023-02-06 19:04:30 +0200

    Update About.js

--- god/src/pages/About.js
+++ god/src/pages/About.js
@@ -1,30 +1,96 @@
-import { Component } from "react";
+import { Component, useEffect } from "react";
+import { useForm } from "react-hook-form";
+import { Link, useNavigate, useParams } from "react-router-dom";
+import axios from "axios";
 import Menu from "./components/Menu";
-class About extends Component {
-    render() {
-        return (
-            <>
-                <Menu />
+export default function About() {
+
+    const { register, formState: { errors }, handleSubmit, reset } = useForm();
+
+    const navigate = useNavigate();
+
+    const { id } = useParams()
+
+
+    useEffect(() => {
+        axios.get(`http://reactjs-front.com/BackEnd-Laravel/public/api/find/${id}`)
+            .then((resultadoAPI) => {
+                reset(resultadoAPI.data)
+            })
+
+            .catch(() => {
+                console.log("API NAO ESTA A RETORNAR NADA PUTO!")
+            })
+
+    }, []);
+
+    const onSubmit = (data) => axios.post("http://reactjs-front.com/BackEnd-Laravel/public/api/salvar", data)
+        .then(() => {
+            navigate('/');
+            console.log("deu tudoe certo")
+        })
+        .catch(() => {
+            console.log("Tudo errado")
+        })
+    return (
+        <>
+            <Menu />
+            <div className="container ">
                 <div className="container">
                     <div className=" d-flex justify-content-center">
                         <div className="row col-md-6 col-sm-6 py-4 ">
                             <div className="col-12 ">
                                 <div className="card ">
                                     <div className="card-header">
-                                        <strong>Consumindo APi</strong>
+                                        <strong>Criar dados</strong><Link to={'/'} className="btn btn-info btn-sm float-end">Voltar</Link>
                                         <hr></hr>
-                                        <p>#2- Nessa vídeo aula daremos continuidade criando os componentes da tela de Login e criaremos as funções responsáveis pelo login utilizando Axios e Redux.
-                                        </p>
+                                        <form onSubmit={handleSubmit(onSubmit)}>
+                                            <div className="input-group mb-3">
+                                                <span className="input-group-text" id="basic-addon1">Nome</span>
+                                                <input type="text" {...register("nome", { required: true })} className="form-control" placeholder="Username" aria-label="Username" />
+                                            </div>
+                                            <span className="text-danger">
+                                                {errors.nome?.type === 'required' && <p role="alert">Nome e obrigatorio</p>}
+                                            </span>
 
+                                            <div className="input-group mb-3">
+                                                <span className="input-group-text" id="basic-addon1">Curso</span>
+                                                <input type="text" {...register("curso", { required: true })} className="form-control" placeholder="Username" aria-label="Username" />
+                                            </div>
+                                            <span className="text-danger">
+                                                {errors.curso?.type === 'required' && <p role="alert">Curso e obtigatorio</p>}
+                                            </span>
+
+                                            <div className="input-group mb-3">
+                                                <span className="input-group-text" id="basic-addon1">Email</span>
+                                                <input type="text" {...register("email", { required: true })} className="form-control" placeholder="Username" aria-label="Username" />
+                                            </div>
+                                            <span className="text-danger">
+                                                {errors.email?.type === 'required' && <p role="alert">O email e obrigatorio</p>}
+                                            </span>
+
+                                            <div className="input-group mb-3">
+                                                <span className="input-group-text" id="basic-addon1">Phone</span>
+                                                <input type="text"
+                                                    {...register("telefone", { required: "Etelefone Address is required" })}
+                                                    aria-invalid={errors.telefone ? "true" : "false"}
+
+                                                    className="form-control" placeholder="Username" aria-label="Username" />
+                                            </div>
+                                            <span className="text-danger">
+                                                {errors.telefone?.type === 'required' && <p role="alert">O Campo Telefone e de carrater obrigatorio</p>}
+                                            </span>
+
+
+                                            <button type="submit" className="btn btn-success btn-block ">Salvar no laravel</button>
+                                        </form>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </>
-        );
-    }
+            </div>
+        </>
+    );
 }
-
-export default About;
