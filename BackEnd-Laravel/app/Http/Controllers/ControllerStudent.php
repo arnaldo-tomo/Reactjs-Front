9f031d8cece1c80154df82b501b534ccfbae2302
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2023-02-06 19:49:15 +0200

    Update ControllerStudent.php

--- BackEnd-Laravel/app/Http/Controllers/ControllerStudent.php
+++ BackEnd-Laravel/app/Http/Controllers/ControllerStudent.php
@@ -32,4 +32,25 @@ class ControllerStudent extends Controller
         $student = student::find($id);
         return  response()->json($student);
     }
+    public function delete($id)
+    {
+        $student = student::find($id);
+        $student->delete();
+        return  response()->json($student);
+    }
+
+    public function update(Request $request)
+    {
+        $dado = student::find($request->id);
+        $dado->nome = $request->nome;
+        $dado->curso = $request->curso;
+        $dado->email = $request->email;
+        $dado->telefone = $request->telefone;
+        $dado->update();
+
+        return  response()->json([
+            'status' => 200,
+            'message' => 'Estudante Salvo Com Sucessos',
+        ]);
+    }
 }
