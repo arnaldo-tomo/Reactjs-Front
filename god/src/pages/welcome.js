43e7f3e80094d517c756429f2108982e118ef4a5
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2023-02-06 09:18:02 +0200

    Update welcome.js

--- god/src/pages/welcome.js
+++ god/src/pages/welcome.js
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Menu from "./components/Menu";
+import { Link } from 'react-router-dom';
+
+
 export default function Welcome() {
 
     let [dado, Dados] = useState([]);
@@ -21,21 +24,21 @@ export default function Welcome() {
     return (
         <>
             <Menu />
-
             {dado.map((pessoa, key) => {
                 return (
-                    <div className="container">
+                    <div className="container" key={key}>
                         <div className=" d-flex justify-content-center">
                             <div className="row col-md-6 col-sm-6  ">
                                 <div className="col-12  py-2">
                                     <div className="card ">
                                         <div className="card-header">
-                                            <strong>Consumindo APi</strong>
+                                            <strong>ID:{pessoa.id} - {pessoa.nome}</strong>
                                             <hr></hr>
-                                            <p>#2- Nessa vídeo aula daremos continuidade criando os componentes da tela de Login e criaremos as funções responsáveis pelo login utilizando Axios e Redux.
-                                            </p>
+                                            <p>Curso: {pessoa.curso}</p>
+                                            <p>Email: {pessoa.email}</p>
+                                            <p>Telefone: {pessoa.telefone}</p>
                                             <div className="m-2 d-flex justify-content-between">
-                                                <a className="btn btn-success btn-sm m-2">EDITAR</a>
+                                                <Link to={{ pathname: `/about/${pessoa.id}` }} className="btn btn-success btn-sm m-2">EDITAR</Link>
                                                 <a className="btn btn-info btn-sm m-2">Ler MaIs</a>
                                                 <a className="btn btn-danger btn-sm m-2">Deletar</a>
                                             </div>
