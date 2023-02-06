eef9510a73c464a7bf0cdd2505731873754a86a2
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2023-02-06 19:49:21 +0200

    Update api.php

--- BackEnd-Laravel/routes/api.php
+++ BackEnd-Laravel/routes/api.php
@@ -8,3 +8,5 @@ use Illuminate\Support\Facades\Route;
 Route::post('/salvar', [ControllerStudent::class, 'salvar']);
 Route::get('/todos', [ControllerStudent::class, 'getStude']);
 Route::get('/find/{id}', [ControllerStudent::class, 'find']);
+Route::get('/delete/{id}', [ControllerStudent::class, 'delete']);
+Route::post('update', [ControllerStudent::class, 'update']);
